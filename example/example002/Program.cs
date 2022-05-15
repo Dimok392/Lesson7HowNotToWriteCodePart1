fe1f3c7a691f1be1eab23bc2e17c590bfb3641b6
Пётр <dimok392@mail.ru>
2022-05-15 15:28:55 +0300

    example/example002

---
+++ example/example002/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int m = 3;
+int n = 4;
+
+void Print (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+
+int [,] mass = new int [m,n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j]=i+j;
+    //    Console.Write(mass[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+Print (mass);
